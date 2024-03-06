16066bd9fd8fa3d4dc19ac34f83e77bf5ecfda0d
Pranav Kumar <pranavkumar1522@gmail.com>
2024-03-06 12:53:14 +0530

    DataTypes

---
+++ basics/02_Datatypes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    // int - store a number within a certain range
+    int x = 5;
+    // long long - also store a number but has a great range then int
+    long long y = 23456789003;
+    // float - it stores a decimal value
+    float z = 5.677;
+    // string -  it stores a string value
+    string str = "Hello World";
+    // char - It stores a single character value
+    char ch = 'A';
+    // Boolean - it stores true or false value
+    bool isLoggedIn = true;
+
+    cout << x << endl
+         << y << endl
+         << z << endl
+         << str << endl
+         << ch << endl
+         << isLoggedIn << endl;
+         return 0;
+}
